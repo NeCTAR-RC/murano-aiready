f856f3657fa3ea77529c5b9d20e8368e4f895b27
Andy Botting <andy@andybotting.com>
2025-07-23 09:21:05 +1000

    Remove support for external volume home dir handling for now
    Change-Id: If46e631c543abb5e911c8ab23b6e00bcbaceeff3

--- au.org.nectar.AIReady/Resources/scripts/setup.sh
+++ au.org.nectar.AIReady/Resources/scripts/setup.sh
@@ -3,46 +3,6 @@
 USERNAME="$1"
 ORIGUSER="ubuntu"
 
-# Assume last disk is our attached storage,
-# except /dev/vda which is our root disk
-DISK=$(ls /dev/vd[b-z] | tail -n1)
-
-# Check if the disk is mounted (e.g. if ephemeral)
-MOUNTPOINT=$(lsblk -n $DISK -o MOUNTPOINT)
-
-# If we have a disk, but it's not mounted, then it's probably
-# our external volume for home
-if [ ! -z $DISK ] && [ -z $MOUNTPOINT ]; then
-
-	# Have external mount for /home
-	MOUNT="/homevol"
-
-	# Partition label
-	if [ "$(lsblk -n -o PARTTYPE $DISK)" == "" ]; then
-		parted $DISK mklabel msdos
-	fi
-
-	# Partition
-	if ! lsblk -n $DISK | grep -q " part "; then
-		parted -a opt $DISK mkpart primary ext4 0% 100%
-	fi
-
-	# Filesystem
-	if [ "$(lsblk -n -o FSTYPE ${DISK}1)" == "" ]; then
-		mkfs.ext4 ${DISK}1
-	fi
-
-	# Mount volume
-	if ! mount | grep -q $MOUNT; then
-		mkdir -p $MOUNT
-		echo "${DISK}1 $MOUNT ext4 defaults 0 2" >> /etc/fstab
-		mount $MOUNT
-	fi
-else
-	# Use regular /home if no volume is found
-	MOUNT="/home"
-fi
-
 if [ "$USERNAME" != "$ORIGUSER" ]; then
 
   ORIGHOME=$(eval echo "~$ORIGUSER" )
